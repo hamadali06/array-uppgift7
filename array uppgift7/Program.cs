a47b142b0691172ca58c7fd23e1935e1ffe50483
Emad Iddin Ali <ea224pn@student.lnu.se>
2024-06-05 19:14:19 +0200

    Add project files.

---
+++ array uppgift7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Inlamning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] länder = { "Kina", "Indien", "USA", "Indonesien", "Pakistan" };
+            for (int i = 0; i < länder.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {länder[i]}");
+            }
+            Console.WriteLine("Ange nummer på länder att visa igen (separerade med mellanslag):");
+            string[] val = Console.ReadLine().Split(' ');
+            foreach (string s in val)
+            {
+                int index = int.Parse(s) - 1;
+                Console.WriteLine($"{index + 1}. {länder[index]}");
+            }
+        }
+    }
+}
